f7802e513b4f419300138bd011a34c2287ddb5f0
fandeka <fandeka.aghfirlana@gmail.com>
2018-11-07 07:50:02 +0700

    - dashboard - modul user (tambah dan daftar pengguna)

---
+++ application/views/admin/common/common_header_top.php
@@ -0,0 +1,50 @@
+    <div class="header-top-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                    <div class="logo-area">
+                        <a href="#"><img src="img/logo/logo.png" alt="" /></a>
+                    </div>
+                </div>
+                <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12">
+                    <div class="header-top-menu">
+                        <ul class="nav navbar-nav notika-top-nav">
+
+                            <li class="nav-item nc-al"><a href="#" data-toggle="dropdown" role="button" aria-expanded="false" class="nav-link dropdown-toggle"><span><i class="notika-icon notika-menus"></i></span></a>
+                                <div role="menu" class="dropdown-menu message-dd notification-dd animated zoomIn">
+                                    <div class="hd-mg-tt">
+                                        <h2></h2>
+                                    </div>
+                                    <div class="hd-message-info">
+                                        <a href="<?php echo site_url("users/change_password") ?>">
+                                            <div class="hd-message-sn">
+                                                <div class="hd-message-img">
+                                                    <img src="img/post/1.jpg" alt="" />
+                                                </div>
+                                                <div class="hd-mg-ctn">
+                                                    <h3>Ubah Password</h3>
+                                                    <p>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</p>
+                                                </div>
+                                            </div>
+                                        </a>
+                                        <a href="<?php echo site_url("admin/signout") ?>">
+                                            <div class="hd-message-sn">
+                                                <div class="hd-message-img">
+                                                    <img src="img/post/2.jpg" alt="" />
+                                                </div>
+                                                <div class="hd-mg-ctn">
+                                                    <h3>Logout</h3>
+                                                    <p>Keluar dari system &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</p>
+                                                </div>
+                                            </div>
+                                        </a>
+                                    </div>
+
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
---
+++ application/views/common/common_head.php
@@ -0,0 +1,56 @@
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Pyshical Fitness Test Information System</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- favicon
+        ============================================ -->
+    <link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico">
+    <!-- Google Fonts
+        ============================================ -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,700,900" rel="stylesheet">
+    <!-- Bootstrap CSS
+        ============================================ -->
+    <link rel="stylesheet" href="<?php echo base_url($this->config->item("theme_admin")."/css/bootstrap.min.css"); ?>"> 
+    <!-- font awesome CSS
+        ============================================ -->
+    <link rel="stylesheet" href="<?php echo base_url($this->config->item("theme_admin")."/css/font-awesome.min.css"); ?>">
+    <!-- owl.carousel CSS
+        ============================================ -->
+    <link rel="stylesheet" href="<?php echo base_url($this->config->item("theme_admin")."/css/owl.carousel.css"); ?>">
+    <link rel="stylesheet" href="<?php echo base_url($this->config->item("theme_admin")."/css/owl.theme.css"); ?>">
+    <link rel="stylesheet" href="<?php echo base_url($this->config->item("theme_admin")."/css/owl.transitions.css"); ?>">
+        <!-- meanmenu CSS
+        ============================================ -->
+    <link rel="stylesheet" href="<?php echo base_url($this->config->item("theme_admin")."/css/meanmenu/meanmenu.min.css"); ?>">
+    <!-- animate CSS
+        ============================================ -->
+    <link rel="stylesheet" href="<?php echo base_url($this->config->item("theme_admin")."/css/animate.css"); ?>">
+    <!-- normalize CSS
+        ============================================ -->
+    <link rel="stylesheet" href="<?php echo base_url($this->config->item("theme_admin")."/css/normalize.css"); ?>">
+    <!-- mCustomScrollbar CSS
+        ============================================ -->
+    <link rel="stylesheet" href="<?php echo base_url($this->config->item("theme_admin")."/css/scrollbar/jquery.mCustomScrollbar.min.css"); ?>">
+    <!-- wave CSS
+        ============================================ -->
+    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="<?php echo base_url($this->config->item("theme_admin")."/css/wave/waves.min.css"); ?>">
+    <!-- Notika icon CSS
+        ============================================ -->
+    <link rel="stylesheet" href="<?php echo base_url($this->config->item("theme_admin")."/css/notika-custom-icon.css"); ?>">
+    <!-- main CSS
+        ============================================ -->
+    <link rel="stylesheet" href="<?php echo base_url($this->config->item("theme_admin")."/css/main.css"); ?>">
+
+    <!-- style CSS
+        ============================================ -->
+    <link rel="stylesheet" href="<?php echo base_url($this->config->item("theme_admin")."/style.css"); ?>">
+    <!-- responsive CSS
+        ============================================ -->
+    <link rel="stylesheet" href="<?php echo base_url($this->config->item("theme_admin")."/css/responsive.css"); ?>">
+    <!-- modernizr JS
+        ============================================ -->
+    <script src="<?php echo base_url($this->config->item("theme_admin")."/js/vendor/modernizr-2.8.3.min.js"); ?>"></script>
+</head>
