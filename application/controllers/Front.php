78f148c8eac8e34f68c63853c6009f07afcff65d
fandeka <fandeka.aghfirlana@gmail.com>
2018-11-19 22:18:31 +0700

    Menu change password di Front

--- application/controllers/Front.php
+++ application/controllers/Front.php
@@ -15,6 +15,84 @@ class Front extends CI_Controller {
             $this->load->view("front/dashboard",$data);
         }
     }
+
+    function change_password(){
+        if(_is_frontend_user_login($this)){
+            $this->load->model("users_model");
+                $user_data  = $this->users_model->get_user_by_id(_get_current_user_id($this));
+                $data["user_data"] = $user_data;
+
+            if($_POST){
+                
+                $this->load->model("users_model");
+           
+                $this->load->library('form_validation');
+                
+                $this->form_validation->set_rules('c_password', 'Current Password', 'trim|required');
+                $this->form_validation->set_rules('n_password', 'New Password', 'trim|required');
+                $this->form_validation->set_rules('r_password', 'Re Password', 'trim|required');
+                
+                if ($this->form_validation->run() == FALSE) {
+                   
+                    $error_array = $this->form_validation->error_string();
+                    $error_json = json_encode(strip_tags($error_array));
+                    $error_clean = str_replace(['"', '"'], '', $error_json);
+                    $data["error"] =  "<script type='text/javascript'>
+                                                    var teks = ' ".$error_clean."';
+                                                    swal({   
+                                                            title: '',   
+                                                            text: teks,   
+                                                            type: 'warning',   
+                                                            showCancelButton: false,   
+                                                            confirmButtonColor: '#DD6B55',   
+                                                            confirmButtonText: 'OK', 
+                                                            closeOnConfirm: false 
+                                                        });
+                                            </script>";
+                }else {
+                   
+                    if($user_data->user_password == md5($this->input->post("c_password"))){
+                     
+                        $n_password = $this->input->post("n_password");
+                        $r_password = $this->input->post("r_password");
+
+                       
+                        
+                        if($n_password == $r_password){
+                            $this->load->model("common_model");
+                            $resid = $this->common_model->data_update("users",
+                                array("user_password"=>md5($n_password)),array("user_id"=>_get_current_user_id($this)));
+                           
+                            echo  '<script>alert("Data berhasil disimpan...");window.location = "'.site_url().'/users/change_password";</script>
+                                ';
+                            $data["error"] = "<script type='text/javascript'>
+                                                    swal('Sukses!', 'Data berhasil disimpan', 'success')
+                                            </script>";
+                        }
+                        
+                    }else{
+                        $data["error"] = "<script type='text/javascript'>
+                                                    swal({   
+                                                            title: '',   
+                                                            text: 'Password tidak cocok',   
+                                                            type: 'warning',   
+                                                            showCancelButton: false,   
+                                                            confirmButtonColor: '#DD6B55',   
+                                                            confirmButtonText: 'OK', 
+                                                            closeOnConfirm: false 
+                                                    });
+                                         </script>";
+
+                    }         
+                        
+                }
+            }    
+               
+                $this->load->view("front/change_password",$data);
+
+        }
+    }   
+ 
     
 
 	
